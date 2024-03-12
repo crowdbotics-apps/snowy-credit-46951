dabfb2c98658eacf47ebe4a2cff0169fa0c26b57
Crowdbotics <team@crowdbotics.com>
2024-03-12 02:45:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snowy-credit-46951",
+  url: "https://snowy-credit-46951.botics.co",
+  api: "https://snowy-credit-46951.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
